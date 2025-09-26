3a515678a48b7fbb407768fcebb2ad4f22e9bf28
Mauro Miranda <solluzitecnologia@gmail.com>
2025-09-26 16:25:52 -0300

    fix: index.php

--- public/index.php
+++ public/index.php
@@ -1,72 +1,43 @@
 <?php
+declare (strict_types=1);
 
-declare(strict_types=1);
+use Solluzi\Diactoros\Response\SapiEmitter;
+use Solluzi\Diactoros\ServerRequestFactory;
+use Solluzi\Router\AltoRouter;
+use Solluzi\Router\Container\Container;
+use Solluzi\Router\Middleware\CorsMiddleware;
+use Solluzi\Router\Middleware\DispatchMiddleware;
+use Solluzi\Router\Middleware\MethodNotAllowedMiddleware;
+use Solluzi\Router\Middleware\NotFoundHandler;
+use Solluzi\Router\Middleware\UrlHelperMiddleware;
+use Solluzi\Router\Router;
 
-
-use Solluzi\Application\Cors;
-use Solluzi\Application\ErrorDisplay;
-
-/**
- * @author Mauro Joaquim Miranda <mauro.miranda@solluzi.com.br>
- * @license MIT
- * @package public
- * @copyright 2018 Solluzi Tecnologia da Informação LTDA
- */
-
-/**
-*--------------------------------------------------------------------------
-*                       REGISTRA O AUTOLOADER
-*--------------------------------------------------------------------------
-*
-* O Composer fornece um carregador de classes gerado automaticamente e 
-* conveniente para esta aplicação. precisamos utilizá-lo! Vamos 
-* simplesmente chamá-lo automaticamente, desta feita não precisamos 
-* carregar manualmente nossas classes.
-*
-*/
 require dirname(__DIR__, 1) . '/bootstrap.php';
 
+// Carrega as variaveis de ambiente do arquivo .env
 
+$container = new Container();
+$app = new Router($container);
 
-/**
-*--------------------------------------------------------------------------
-*                               CORS
-*--------------------------------------------------------------------------
-*
-* It's necessary, for api to accept outside connections, to set all cors
-* directives, and this part of the code will interpret it.
-* preset option for allowed origins for our API server
-*
-*/
+$container->set(AltoRouter::class, function () use ($app) {
+    return $app->getRouter();
+});
 
-$allowedOrigins   = explode(';', env('ALLOWED_DOMAINS'));
-$allowedMethods   = explode('|', env('ALLOWED_METHODS'));
-$allowedHeaders   = ["Content-Type", "Authorization"];
-$exposeHeaders    = explode('|', env('EXPOSE_HEADERS'));
-$allowCredentials = env('ALLOWED_CREDENTIALS');
+$alllowOrigins = $_ENV['CORS_ALLOWED_ORIGINS'] ?? '*';
 
-$cors = new Cors($allowedOrigins, $allowedMethods, $allowedHeaders, $allowCredentials, $exposeHeaders);
-$cors->handle();
+$allowedOriginsArray = array_map('trim', explode(';', $alllowOrigins));
 
-/**
- *--------------------------------------------------------------------------
- *                          ERROR REPORTING
- *--------------------------------------------------------------------------
- *
- * Different environments will require different levels of error reporting.
- * By default development will show errors but testing and live will hide them.
- *
- */
-new ErrorDisplay(APP_ENVIRONMENT);
+$app->pipe(new CorsMiddleware($allowedOriginsArray));
+$app->pipe(UrlHelperMiddleware::class);
+$app->pipe(new DispatchMiddleware($app->getRouter(), $container));
+$app->pipe(MethodNotAllowedMiddleware::class);
+$app->pipe(NotFoundHandler::class);
 
-/**
- *--------------------------------------------------------------------------
- *                           ROUTES TREATMENT
- *--------------------------------------------------------------------------
- *
- * This section treats all routes and make navigation possible
- *
- */
-require_once APP_CONFIGS . '/routes.php';
+$app->loadRoutes();
 
+$request = ServerRequestFactory::fromGlobals();
 
+$response = $app->run($request);
+
+$emitter = new SapiEmitter();
+$emitter->emit($response);
