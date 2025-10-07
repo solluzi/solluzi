d6a374d1e60be63c0d0ec5b3574589edbfa94d67
Mauro Miranda <solluzitecnologia@gmail.com>
2025-10-07 01:59:46 -0300

    fix: base

--- config/twig.php
+++ config/twig.php
@@ -5,8 +5,20 @@ declare(strict_types=1);
 return [
     'default' => [
         'templates' => [
-            path('app/default'),
+            path('templates'),             // raiz
+            'site'  => path('app/Website/View'),   // @Site
         ],
-        'cache' => []
+        'cache' => path('storage/twig'),
     ],
+    'extensions' => [
+        'global' => [],
+        'filters' => [],
+        'functions' => [],
+        'tokenParsers' => [],
+        'operators' => [
+            'unary'  => [], // ex: ['not' => ['precedence'=>50, 'class'=>MyNotTokenParser::class]]
+            'binary' => [], // ex: ['and' => ['precedence'=>20, 'class'=>MyAndNode::class, 'associativity'=>'left']]
+        ],
+        'tests' => []
+    ]
 ];
--- public/index.php
+++ public/index.php
@@ -3,25 +3,15 @@ declare (strict_types=1);
 
 use Solluzi\Diactoros\Response\SapiEmitter;
 use Solluzi\Diactoros\ServerRequestFactory;
-use Solluzi\Router\AltoRouter;
-use Solluzi\Router\Container\Container;
 use Solluzi\Router\Middleware\CorsMiddleware;
 use Solluzi\Router\Middleware\DispatchMiddleware;
 use Solluzi\Router\Middleware\MethodNotAllowedMiddleware;
 use Solluzi\Router\Middleware\NotFoundHandler;
 use Solluzi\Router\Middleware\UrlHelperMiddleware;
-use Solluzi\Router\Router;
 
 require dirname(__DIR__, 1) . '/bootstrap.php';
 
 // Carrega as variaveis de ambiente do arquivo .env
-
-$container = new Container();
-$app = new Router($container);
-
-$container->set(AltoRouter::class, function () use ($app) {
-    return $app->getRouter();
-});
 
 $alllowOrigins = $_ENV['CORS_ALLOWED_ORIGINS'] ?? '*';
 
